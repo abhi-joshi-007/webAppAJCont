e74b74367d9b545dbf54e124186f9c195d99cb7a
Abhishek Joshi <NPD9@eurofins.com>
2020-07-20 17:37:47 +0530

    Add project files.

---
+++ webAppAJCont/Controllers/DetailsController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace webAppAJCont.Controllers
+{
+    public class DetailsController : ApiController
+    {
+        // GET: api/Details
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "abhishekjoshi@eurofins.com", "98989898" };
+        }
+
+        // GET: api/Details/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Details
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Details/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Details/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
